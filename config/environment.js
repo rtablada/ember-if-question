a968ad28d303cf5cf4c4bdacb091d2b6d4ae6fb1
Ryan Tablada <ryan.tablada@gmail.com>
2021-02-11 10:41:22 -0600

    codesandbox

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   let ENV = {
-    modulePrefix: 'foobar',
+    modulePrefix: 'my-app',
     environment,
     rootURL: '/',
     locationType: 'auto',
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,7 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
+    name: 'my-app',
     // Add options here
   });
 
